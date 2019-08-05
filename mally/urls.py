bbecfed51d105552420353a84ca055ad702974d0
ElimuStuart <stuartelimu@gmail.com>
2019-08-06 00:34:28 +0300

    frontend setup

--- mally/urls.py
+++ mally/urls.py
@@ -17,10 +17,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('shop.urls', namespace='shop')),
+    path('', views.index),
+    path('about/', views.about),
+    path('contact/', views.contact),
 ]
 
 if settings.DEBUG:
