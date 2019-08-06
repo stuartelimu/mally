e7abbb7824a554bfdc252106338ffb75295001b1
ElimuStuart <stuartelimu@gmail.com>
2019-08-06 09:16:32 +0300

    shop page displaying all the products

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Order, OrderItem, Item
 
-# Register your models here.
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Order)
+admin.site.register(OrderItem)
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from .models import Item
+
 # Create your views here.
 def shop(request):
-    return render(request, "shop/shop.html")
+    items = Item.objects.order_by('-created')
+    context = {
+        'items': items
+    }
+    return render(request, "shop/shop.html", context)
