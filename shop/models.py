a16612d3e6d213a679102a2700b407a948b8c9ee
ElimuStuart <stuartelimu@gmail.com>
2019-08-06 08:58:44 +0300

    created models for shop

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Item(models.Model):
+    title = models.CharField(max_length=25)
+    price = models.FloatField()
+    slug = models.SlugField(max_length=250, unique_for_date='created')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
 
 
-# Create your models here.
+class OrderItem(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.item.title
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    created = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField()
+    ordered = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
+
