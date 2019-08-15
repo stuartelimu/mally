02fcd573b621d0e679ac2716f7d1800edad34b4f
ElimuStuart <stuartelimu@gmail.com>
2019-08-15 10:22:59 +0300

    db setup for location based app

--- mally/settings.py
+++ mally/settings.py
@@ -91,8 +91,10 @@ WSGI_APPLICATION = 'mally.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),,
+        'PASSWORD': config('DB_PASSWORD'),
     }
 }
 
