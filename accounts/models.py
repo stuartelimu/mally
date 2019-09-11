82b6d0907da9c43e5fa1fc2f178360707f6888ce
ElimuStuart <stuartelimu@gmail.com>
2019-09-11 20:19:17 +0300

    create Profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"Profile for {self.user.username}"
