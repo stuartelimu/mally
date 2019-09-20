e2437014eecd1205c24239004ee6da555b34170c
ElimuStuart <stuartelimu@gmail.com>
2019-09-20 07:12:22 +0300

    add address details to the user profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from shop.models import Order
+
 
 # Create your views here.
 def profile(request):
-    return render(request, 'accounts/profile.html')
+    last_order = Order.objects.filter(user=1, ordered=True).order_by('-ordered_date')[0]
+    return render(request, 'accounts/profile.html', {'last_order':last_order})
     
