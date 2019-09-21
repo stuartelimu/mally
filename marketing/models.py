d081888fed9bc5b59c9bc2dc06c635cc30f87e98
ElimuStuart <stuartelimu@gmail.com>
2019-09-21 23:11:28 +0300

    create newsletter model

--- marketing/models.py
+++ marketing/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
+
 
 # Create your models here.
+class NewsLetter(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    email = models.EmailField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
